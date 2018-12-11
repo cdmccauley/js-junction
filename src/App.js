4b6006eee322196e6e33a71503f35dbc4bbde26c
Chris McCauley <cdmccauley@gmail.com>
2018-12-10 18:53:06 -0700

    switched to using anon auth to start, interacting with db now

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import firebase from 'firebase';
 // import firebase from 'firebase';
 // import 'firebase/PACKAGE';
 
-// firebase config declaration
-let firebaseConfig = {
+// firebase declarations
+let firebaseAppConfig = {
   apiKey: 'AIzaSyDc-6kYnf2feWrzCRTQ6vDIfkC22qjdfUo',
   authDomain: 'js-junction.firebaseapp.com',
   databaseURL: 'https://js-junction.firebaseio.com',
@@ -17,77 +17,51 @@ let firebaseConfig = {
   storageBucket: 'js-junction.appspot.com',
   messagingSenderId: '646306703866',
 };
+let firebaseDbConfig = {
+  timestampsInSnapshots: true,
+}
+let firebaseDb;
 
 // firebase init
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseAppConfig);
+firebaseDb = firebase.firestore();
+firebaseDb.settings(firebaseDbConfig);
 
-// TODO: read more on https://firebase.google.com/docs/auth/web/email-link-auth for gotchas
 class App extends Component {
 
   constructor() {
     super();
 
     this.state = {
-      
+      user: null,
+      uid: null,
     };
-
-    this.txtInput = React.createRef();
-
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
-    // check if url is a login request
-    if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
-      // get stored email if present
-      let email = window.localStorage.getItem('emailForSignIn');
-      if (!email) {
-        // prompt for email if storage returned falsy
-        email = window.prompt('Please provide your email for confirmation.');
-      }
-      // send login request to firebase
-      firebase.auth().signInWithEmailLink(email, window.location.href).then((result) => {
-        // remove email from storage
-        window.localStorage.removeItem('emailForSignIn');
-        // log user
-        console.log(result.user);
-      })
-      .catch((err) => { console.log(err) });
-    }
-  }
-
-  /*
-  email link format:
-  http://localhost:3000/
-  ?apiKey=AIzaSyDc-6kYnf2feWrzCRTQ6vDIfkC22qjdfUo
-  &oobCode=qBMj0p_pH1jXuNOUUa5NGzZDbjCZKwS2WYA2mPC4wlcAAAFne2jQUQ
-  &mode=signIn
-  &lang=en
-  */
-  handleSubmit(e) {
-    // prevent reload
-    e.preventDefault();
-    // log ref val
-    console.log(this.txtInput.current.value);
-    // declarations for firebase.auth
-    let actionCodeSettings = {
-      url: 'http://localhost:3000',
-      handleCodeInApp: true,
-    }
-    let email = 'cdmccauley@gmail.com';
-    // send email with link
-    firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
-    .then(() => { window.localStorage.setItem('emailForSignIn', email) })
-    .catch((err) => { console.log(err.code) });
+    // setup auth observer
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        firebaseDb.collection('users').add({ uid: user.uid }).then((docRef) => console.log('document written with id: ', docRef.id)).catch((err) => console.log(err));
+        this.setState((state, props) => {
+          return {
+            user: user,
+            uid: user.uid,
+          };
+        });
+      } else {
+        // ??
+      };
+      console.log('this.state.user:\n', this.state.user);
+    });
+    // login user anonymously
+    firebase.auth().signInAnonymously().catch((err) => console.log(err));
   }
 
   render() {
     return (
       <React.Fragment>
-        <form onSubmit={ this.handleSubmit }>
-          <input type='text' ref={ this.txtInput }></input>
-          <input type='submit' value='login' />
-        </form>
+        <p>{ this.state.uid }</p>
       </React.Fragment>
     );
   }
